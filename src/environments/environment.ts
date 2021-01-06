c22765fea9df35799c805ad689eebf4e794e600f
Invitado Ocirio <xav.xgca@gmail.com>
2021-01-05 22:51:51 -0500

    Add login with google

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCngkqTRNaxZcWd54vSm1nrgYEoP2eva-Q",
+    authDomain: "milogin-d1f4c.firebaseapp.com",
+    projectId: "milogin-d1f4c",
+    storageBucket: "milogin-d1f4c.appspot.com",
+    messagingSenderId: "31060928206",
+    appId: "1:31060928206:web:6cec77dd33ba878d1fd29f"
+  }};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
